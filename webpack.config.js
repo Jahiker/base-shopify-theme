b7d1395fc0028782b7999de6e12c6023425cc3e9
Jahiker <rojasjahiker@gmail.com>
2025-12-06 19:03:59 -0400

    feat: Integrate Tailwind CSS and update Webpack configuration for asset bundling.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './assets/main.js',
+  entry: {
+    main: './assets/main.js',
+    bundle: './assets/base.css',
+  },
   output: {
-    filename: '[name].min.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'assets'),
   },
-  mode: 'production',
   module: {
     rules: [
       {
@@ -14,8 +17,24 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
 };
